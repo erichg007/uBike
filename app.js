c4b1a4a8dd26cf0c6efc428ed00b7319032d5fea
erichg007 <chishiou0609@gmail.com>
2022-03-25 01:24:40 +0800

    code reduction

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 程式碼寫這裡
-//刪除前次查詢的列表
-function oldListDelete() {
+//刪除前次查詢的列表函數
+function queryResultClear() {
   const oldList = document.querySelector(".siteList")
   oldList.remove()
   const newList = document.createElement("ul")
@@ -8,36 +8,52 @@ function oldListDelete() {
   const main = document.querySelector(".container")
   main.insertAdjacentElement("beforeend", newList)
 }
+//建立DOM函數
+function domCreation(tag, tagName, tagcontent) {
+  const object = document.createElement(tag)
+  object.className = tagName
+  object.textContent = tagcontent
+  return object
+}
+//搜尋結果載入函數
+function loadQueryResult(filterResult) {
+  const newLi = domCreation(
+    "li",
+    "list-group-item fs-5",
+    filterResult.split("|")[0].concat("(", filterResult.split("|")[1], ")")
+  )
+  const newImage = domCreation("i", "fas fa-bicycle")
+  newLi.insertAdjacentElement("afterbegin", newImage)
+  const newBr = document.createElement("br")
+  newLi.appendChild(newBr)
+  const newSmall = domCreation(
+    "small",
+    "text-muted",
+    filterResult.split("|")[2]
+  )
+  newLi.appendChild(newSmall)
+  const newList = document.querySelector(".siteList")
+  newList.insertAdjacentElement("beforeend", newLi)
+}
+
 //下載youbike資料，將會用到的站點、數量和地址整理成一個陣列,並建立一個新的列表將被keyword搜尋的資料填入
-async function ubikeData() {
-  const API =
-    "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+async function ubikeData(API, inputValue) {
   const rawData = await fetch(API)
-  const transData = await rawData.json()
+  const transDataSet = await rawData.json()
   const accList = []
-  transData.forEach((x, i) => {
-    let tempList = [x.sna.split("_")[1], x.tot, x.ar]
-    accList[i] = tempList.join("|")
+  transDataSet.forEach((transData, i) => {
+    accList[i] = [
+      transData.sna.split("_")[1],
+      transData.tot,
+      transData.ar,
+    ].join("|")
   })
-  const inputValue = document.querySelector("#searchKeyword").value
-  const filterResult = accList.filter(function (x) {
-    return x.split(inputValue).length > 1
+
+  const filterResults = accList.filter(function (filterResult) {
+    return filterResult.split(inputValue).length > 1
   })
-  filterResult.forEach((x) => {
-    const newLi = document.createElement("li")
-    newLi.className = "list-group-item fs-5"
-    const newImage = document.createElement("i")
-    newImage.className = "fas fa-bicycle"
-    newLi.textContent = x.split("|")[0].concat("(", x.split("|")[1], ")")
-    newLi.insertAdjacentElement("afterbegin", newImage)
-    const newBr = document.createElement("br")
-    newLi.appendChild(newBr)
-    const newSmall = document.createElement("small")
-    newSmall.className = "text-muted"
-    newSmall.textContent = x.split("|")[2]
-    newLi.appendChild(newSmall)
-    const newList = document.querySelector(".siteList")
-    newList.insertAdjacentElement("beforeend", newLi)
+  filterResults.forEach((filterResult) => {
+    loadQueryResult(filterResult)
   })
 }
 
@@ -45,6 +61,9 @@ async function ubikeData() {
 const form = document.querySelector("#searchForm")
 form.addEventListener("submit", (e) => {
   e.preventDefault()
-  oldListDelete()
-  ubikeData()
+  queryResultClear()
+  const API =
+    "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+  const inputValue = document.querySelector("#searchKeyword").value
+  ubikeData(API, inputValue)
 })
